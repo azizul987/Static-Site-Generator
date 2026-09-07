eeba8341d886ba91cf3523b9334c8fc2cf03b05b
azizul987 <wawanajah954@gmail.com>
2026-09-07 07:47:47 +0700

    add new funcion for parsing md

--- src/nodes_delimite.py
+++ src/nodes_delimite.py
@@ -1,6 +1,18 @@
+from tkinter.constants import E
 from xxlimited import new
 
 from textnode import TextNode, TextType
+
+from enum import Enum
+
+class BlockType(Enum):
+    PARAGRAPH = "paragraph"
+    HEADING = "heading"
+    CODE = "code"
+    QUOTE = "quote"
+    UNORDERED_LIST = "unordered_list"
+    ORDERED_LIST = "ordered_list"
+
 
 def split_nodes_delimiter(old_nodes: list[TextNode], delimiter: str, text_type: TextType) -> list[TextNode]:
     new_nodes = []
@@ -16,3 +28,58 @@ def split_nodes_delimiter(old_nodes: list[TextNode], delimiter: str, text_type: 
         else:
             new_nodes.append(node)
     return new_nodes
+
+def split_nodes_image(old_nodes: list[TextNode]) -> list[TextNode]:
+    new_nodes = []
+    for node in old_nodes:
+        if node.text_type == TextType.TEXT:
+            text = node.text
+            if "!" in text:
+                parts = text.split("!")
+                for i, part in enumerate(parts):
+                    new_nodes.append(TextNode(part, TextType.TEXT if i % 2 == 0 else text_type))
+            else:
+                new_nodes.append(node)
+        else:
+            new_nodes.append(node)
+    return new_nodes
+
+def split_nodes_link(old_nodes: list[TextNode]) -> list[TextNode]:
+    new_nodes = []
+    for node in old_nodes:
+        if node.text_type == TextType.TEXT:
+            text = node.text
+            if "[" in text:
+                parts = text.split("[")
+                for i, part in enumerate(parts):
+                    new_nodes.append(TextNode(part, TextType.TEXT if i % 2 == 0 else text_type))
+            else:
+                new_nodes.append(node)
+        else:
+            new_nodes.append(node)
+    return new_nodes
+
+def text_to_textnodes(text:str):
+    nodes = [TextNode(text, TextType.TEXT)]
+    nodes = split_nodes_image(nodes)
+    nodes = split_nodes_link(nodes)
+    return nodes
+
+def markdown_to_blocks(markdown):
+    blocks=markdown.split("\n\n")
+    blocks = [block.strip() for block in blocks if block.strip()]
+    return blocks
+
+def block_to_block_type(block):
+    if block.startswith("# "):
+        return BlockType.HEADING
+    elif block.startswith("```") and block.endswith("```"):
+        return BlockType.CODE
+    elif block.startswith(">"):
+        return BlockType.QUOTE
+    elif block.startswith("* ") or block.startswith("- "):
+        return BlockType.UNORDERED_LIST
+    elif block.startswith("1. "):
+        return BlockType.ORDERED_LIST
+    else:
+        return BlockType.PARAGRAPH
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,7 @@
 from htmlnode import HtmlNode, LeafNode
 from nodes_delimite import split_nodes_delimiter
 from R import extract_markdown_images, extract_markdown_links
+from nodes_delimite import split_nodes_image, split_nodes_link, text_to_textnodes, markdown_to_blocks, block_to_block_type
 def test_htmlnode():
     node = HtmlNode("div", "Hello, World!", None, None)
     assert node.to_html() == "<div>Hello, World!</div>"
@@ -65,3 +66,119 @@ def test_extract_markdown_images(self):
         "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png)"
     )
     self.assertListEqual([("image", "https://i.imgur.com/zjjcJKZ.png")], matches)
+
+def test_split_images(self):
+    node = TextNode(
+        "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png) and another ![second image](https://i.imgur.com/3elNhQu.png)",
+        TextType.TEXT,
+    )
+    new_nodes = split_nodes_image([node])
+    self.assertListEqual(
+        [
+            TextNode("This is text with an ", TextType.TEXT),
+            TextNode("image", TextType.IMAGE, "https://i.imgur.com/zjjcJKZ.png"),
+            TextNode(" and another ", TextType.TEXT),
+            TextNode("second image", TextType.IMAGE, "https://i.imgur.com/3elNhQu.png"),
+        ],
+        new_nodes,
+    )
+
+def test_split_links(self):
+    node = TextNode(
+        "This is text with a [link](https://www.example.com) and another [second link](https://www.example.com/second)",
+        TextType.TEXT,
+    )
+    new_nodes = split_nodes_link([node])
+    self.assertListEqual(
+        [
+            TextNode("This is text with a ", TextType.TEXT),
+            TextNode("link", TextType.LINK, "https://www.example.com"),
+            TextNode(" and another ", TextType.TEXT),
+            TextNode("second link", TextType.LINK, "https://www.example.com/second"),
+        ],
+        new_nodes,
+    )
+
+def test_text_to_textnodes(self):
+    nodes = text_to_textnodes(
+        "This is **text** with an ![image](https://i.imgur.com/zjjcJKZ.png) and a [link](https://boot.dev)"
+    )
+    self.assertListEqual(
+        [
+            TextNode("This is ", TextType.TEXT),
+            TextNode("text", TextType.BOLD),
+            TextNode(" with an ", TextType.TEXT),
+            TextNode("image", TextType.IMAGE, "https://i.imgur.com/zjjcJKZ.png"),
+            TextNode(" and a ", TextType.TEXT),
+            TextNode("link", TextType.LINK, "https://boot.dev"),
+        ],
+        nodes,
+    )
+
+def test_markdown_to_blocks(self):
+    blocks = markdown_to_blocks(
+        "This is **bolded** paragraph\n\nThis is another paragraph with *italic* text and `code` here\n\n"
+    )
+    self.assertListEqual(
+        [],
+        blocks,
+    )
+
+def test_block_to_block_type(self):
+    self.assertEqual(
+        BlockType.HEADING,
+        block_to_block_type("# "),
+    )
+    self.assertEqual(
+        BlockType.CODE,
+        block_to_block_type("```"),
+    )
+    self.assertEqual(
+        BlockType.QUOTE,
+        block_to_block_type("> "),
+    )
+    self.assertEqual(
+        BlockType.UNORDERED_LIST,
+        block_to_block_type("* "),
+    )
+    self.assertEqual(
+        BlockType.ORDERED_LIST,
+        block_to_block_type("1. "),
+    )
+    self.assertEqual(
+        BlockType.PARAGRAPH,
+        block_to_block_type(""),
+    )
+
+def test_paragraphs(self):
+    md = """
+This is **bolded** paragraph
+text in a p
+tag here
+
+This is another paragraph with _italic_ text and `code` here
+
+"""
+
+    node = markdown_to_html_node(md)
+    html = node.to_html()
+    self.assertEqual(
+        html,
+        "<div><p>This is <b>bolded</b> paragraph text in a p tag here</p><p>This is another paragraph with <i>italic</i> text and <code>code</code> here</p></div>",
+    )
+
+
+def test_codeblock(self):
+    md = """
+```
+This is text that _should_ remain
+the **same** even with inline stuff
+```
+"""
+
+    node = markdown_to_html_node(md)
+    html = node.to_html()
+    self.assertEqual(
+        html,
+        "<div><pre><code>This is text that _should_ remain\nthe **same** even with inline stuff\n</code></pre></div>",
+    )
